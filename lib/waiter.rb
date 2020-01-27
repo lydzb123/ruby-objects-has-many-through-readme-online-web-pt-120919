1759a76ac01d6238b8603d73a9b72d6f10d9a2bd
Lydia B <lydiabaik@gmail.com>
2020-01-27 23:47:32 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -23,7 +23,7 @@ class Waiter
 
   def best_tipper
     best_tipped_meal = meals.max { |meal_a, meal_b| meal_a.tip <=> meal_b.tip }
-    best_tipped_meal
+    best_tipped_meal.customer
   end
 
 end
