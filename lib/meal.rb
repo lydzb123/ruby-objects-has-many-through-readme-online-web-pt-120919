ab944a1f280c0db2bf064a67a6b93f7883ba7d06
Lydia B <lydiabaik@gmail.com>
2020-01-27 23:36:21 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -4,5 +4,10 @@ class Meal
   @@all = []
 
   def initialize(waiter, customer, total, tip =0)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    @@all << self
 
 end
